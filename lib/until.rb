663d3ef64c02cf793997afd799f13688e9e0df02
Megan McLean <mmclean@protonmail.com>
2017-01-15 07:54:30 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,13 @@
-def using_until
-        levitation_force = 6
-        #your code here
-    
-end
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 17: While and Until Loops
 
+# Define a method, 'using_until', that accepts no parameter(s).
+# Initialise 'levitation_force' at 6.
+# Until 'levitation_force' is equal to 10, print message and increment
+# 'levitation_force' by 1.
+def using_until
+  levitation_force = 6
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
+end
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,13 @@
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 17: While and Until Loops
+
+# Define a method, 'using_while', that accepts no parameter(s).
+# Initialise 'levitation_force' at 6
+# While 'levitation_force' is less than 10, print message and increment
+# 'levitation_force' by 1
 def using_while
 	levitation_force = 6
-	
-	#your code here
+	while levitation_force < 10
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
 end
-
-
