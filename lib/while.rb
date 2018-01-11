aaa552d519e1ae8f36f58a95ed9f936dc76405e3
allinthemind66 <r.nilon92@gmail.com>
2018-01-10 20:51:16 -0500

    Second Looping While Until Commit

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,9 @@
 def using_while
 	levitation_force = 6
-	while levitation_force < 10
-    puts "Wingardium Leviosa"
-    levitation_force += 1
+
 	#your code here
-end
+	while levitation_force < 10 do
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
 end
