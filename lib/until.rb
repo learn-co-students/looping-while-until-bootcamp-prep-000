655bed615dccaaa10700660889de0ebcfbf41e6d
Brittany Russell <sincerelybr@gmail.com>
2020-02-27 04:13:27 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,16 @@
 def using_until
   levitation_force = 6
   #your code here
+  
+# using until loop 
+# here do is optional 
+  until levitation_force == 10 do
+  
+  # code to be executed 
+  puts levitation_force + 1
+  levitation_force = levitation_force + 1
     
-end
+# here loop ends 
+end 
+
 
